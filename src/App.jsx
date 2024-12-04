a373ad0b2be132bbd55d59851b31380b30f42fad
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-12-04 09:06:18 -0500

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello world</h1>
+     <h1>Testing DOM rendering</h1>
+     <img src={reactLogo} alt='React logo'/>
+    </>
+  )
+}
+
+
+export default App
